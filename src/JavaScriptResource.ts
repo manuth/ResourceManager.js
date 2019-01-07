6dec2771d0b17daa3673e9fe4046fc848720506b
Manuel Thalmann <m@nuth.ch>
2019-01-07 23:29:10 +0100

    Add a class for representing JavaScript-resources

---
+++ src/JavaScriptResource.ts
@@ -0,0 +1,28 @@
+import Path = require("path");
+import { CultureInfo } from ".";
+import { FileResource } from "./FileResource";
+
+/**
+ * Represents a resource which bases on a `.js`-file.
+ */
+export class JavaScriptResource extends FileResource
+{
+    /**
+     * Initializes a new instance of the `JavaScriptResource` class.
+     *
+     * @param fileName
+     * The path to the resource-file.
+     *
+     * @param locale
+     * The locale of the resource.
+     */
+    public constructor(fileName: string, locale?: CultureInfo)
+    {
+        super(fileName, locale);
+    }
+
+    protected Load()
+    {
+        return require(Path.resolve(this.FileName));
+    }
+}
