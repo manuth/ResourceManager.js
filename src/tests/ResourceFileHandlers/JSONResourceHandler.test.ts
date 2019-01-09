3bcf4c3d0b3d61cc49d4c8a4758be767249225a7
Manuel Thalmann <m@nuth.ch>
2019-01-09 23:25:06 +0100

    Rework the tests of the JSONResourceHandler

--- src/tests/ResourceFileHandlers/JSONResourceHandler.test.ts
+++ src/tests/ResourceFileHandlers/JSONResourceHandler.test.ts
@@ -1,5 +1,7 @@
 import Assert = require("assert");
+import { TempFile } from "temp-filesystem";
 import { IResourceFileHandler } from "../../IResourceFileHandler";
+import { JSONResource } from "../../JSONResource";
 import { JSONResourceHandler } from "../../JSONResourceHandler";
 
 suite(
@@ -7,11 +9,43 @@ suite(
     () =>
     {
         let fileHandler: IResourceFileHandler;
+        let jsonFile: TempFile;
+        let jsoncFile: TempFile;
+        let txtFile: TempFile;
+        let yamlFile: TempFile;
 
         suiteSetup(
             () =>
             {
                 fileHandler = new JSONResourceHandler();
+                jsonFile = new TempFile(
+                    {
+                        postfix: ".json"
+                    });
+
+                jsoncFile = new TempFile(
+                    {
+                        postfix: ".jsonc"
+                    });
+
+                txtFile = new TempFile(
+                    {
+                        postfix: ".txt"
+                    });
+
+                yamlFile = new TempFile(
+                    {
+                        postfix: ".yml"
+                    });
+            });
+
+        suiteTeardown(
+            () =>
+            {
+                jsonFile.Dispose();
+                jsoncFile.Dispose();
+                txtFile.Dispose();
+                yamlFile.Dispose();
             });
 
         suite(
@@ -26,6 +60,19 @@ suite(
                         Assert.strictEqual(fileHandler.CheckApplicability("test.jsonc"), true);
                         Assert.strictEqual(fileHandler.CheckApplicability("test.txt"), false);
                         Assert.strictEqual(fileHandler.CheckApplicability("test.yml"), false);
+                    });
+            });
+
+        suite(
+            "Create(string fileName, CultureInfo locale)",
+            () =>
+            {
+                test(
+                    "Checking whether the created resources have the correct type…",
+                    () =>
+                    {
+                        Assert.strictEqual(fileHandler.Create(jsonFile.FullName) instanceof JSONResource, true);
+                        Assert.strictEqual(fileHandler.Create(jsoncFile.FullName) instanceof JSONResource, true);
                     });
             });
     });
