379333cedfc4d96f218a19497b7d889915ca2b36
Manuel Thalmann <m@nuth.ch>
2019-01-12 23:59:17 +0100

    Add a class for resource-managers with mustaches

---
+++ src/MustacheResourceManager.ts
@@ -0,0 +1,47 @@
+import { CultureInfo } from "culture-info";
+import Mustache = require("mustache");
+import { IResourceManager } from "./IResourceManager";
+import { ResourceManagerContext } from "./ResourceManagerContext";
+
+/**
+ * Represents a resource-manager which provides the functionality to interpret mustache-templates in resource-items.
+ */
+export class MustacheResourceManager implements IResourceManager
+{
+    /**
+     * The resource-manager which serves the resource-items.
+     */
+    private resourceManager: IResourceManager;
+
+    public get Locale()
+    {
+        return this.ResourceManager.Locale;
+    }
+
+    public set Locale(value)
+    {
+        this.ResourceManager.Locale = value;
+    }
+
+    /**
+     * Gets the resource-manager which serves the resource-items.
+     */
+    public get ResourceManager()
+    {
+        return this.resourceManager;
+    }
+
+    public Get<T>(name: string, locale?: CultureInfo): T
+    {
+        let result = this.ResourceManager.Get<T>(name, locale);
+
+        if (typeof result === "string")
+        {
+            return Mustache.render(result, new ResourceManagerContext(this, locale)) as any;
+        }
+        else
+        {
+            return result;
+        }
+    }
+}
