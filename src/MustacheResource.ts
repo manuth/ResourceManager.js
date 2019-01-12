bb0c48d0508b1d743251b053d1f0154bc3a958f3
Manuel Thalmann <m@nuth.ch>
2019-01-12 23:05:11 +0100

    Add a class for wrapping resources with mustache

---
+++ src/MustacheResource.ts
@@ -0,0 +1,57 @@
+import Mustache = require("mustache");
+import { IResource } from "./IResource";
+import { ResourceContext } from "./ResourceContext";
+
+/**
+ * Represents a resource which provides the functionality to interpret mustache-templates in resource-items.
+ */
+export class MustacheResource implements IResource
+{
+    /**
+     * The resource which contains the resource-items.
+     */
+    private resource: IResource;
+
+    /**
+     * Initializes a new instance of the `MustacheResource`.
+     *
+     * @param resource
+     * The resource which contains the resource-items.
+     */
+    public constructor(resource: IResource)
+    {
+        this.resource = resource;
+    }
+
+    public get Locale()
+    {
+        return this.Resource.Locale;
+    }
+
+    /**
+     * Gets the resource which contains the resource-items.
+     */
+    public get Resource()
+    {
+        return this.resource;
+    }
+
+    public Get<T>(name: string): T
+    {
+        let result = this.Resource.Get<T>(name);
+
+        if (typeof result === "string")
+        {
+            return Mustache.render(result, new ResourceContext(this)) as any;
+        }
+        else
+        {
+            return result;
+        }
+    }
+
+    public Exists(name: string): boolean
+    {
+        return this.Resource.Exists(name);
+    }
+}
